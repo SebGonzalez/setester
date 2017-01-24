d989ef71381bdc1e6e7f47602f7feeeee077217d
Gabriel Curinga <gabriel.curinga@etu.unice.fr>
2017-01-24 19:30:27 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+ 
+	<link rel="stylesheet" href="font/css/font-awesome.min.css" /> <!-- inclusion des icones -->	
+
+	
+</head>
+<body>
+
+   
+	
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+
+
+</body> 
+</html>
