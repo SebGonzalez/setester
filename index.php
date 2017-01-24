7f4624431bdf2c2b768d50fda99ec4221ab920c0
Gabriel Curinga <gabriel.curinga@etu.unice.fr>
2017-01-24 21:03:38 +0100

    modification car bugs

--- index.php
+++ index.php
@@ -4,15 +4,21 @@
     <meta charset="utf-8">
     <title>Accueil</title>
  
-	<link rel="stylesheet" href="css/font/font-awesome.min.css" /> <!-- inclusion des icones -->	
+	<link rel="stylesheet" href="css/font/css/font-awesome.min.css" /> <!-- inclusion des icones -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" /> <!-- fichier boostrap -->
+	<link rel="stylesheet" href="css/style_menu.css" />
 
-	
 </head>
 <body>
+	<?php
+		include("vue/menu.php");
+	?>
+
 
    
 	
 <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 
 
 </body> 
---
+++ vue/menu.php
@@ -0,0 +1,16 @@
+<body>
+
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <a class="navbar-brand" href="#">
+			<img class="icone"src="img/logo.png" alt="Brand" >
+		  </a>
+		</div>
+	  </div>
+	</nav>
+
+   
+
+   
+</body> 
